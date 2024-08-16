4f671711ff023873b563916f9a17c0b946f74f72
aarya <ilanchea@wlhs.wlwv.k12.or.us>
2024-08-15 23:41:24 -0700

    Finished fetching remote maven repos from json

--- get-deps.py
+++ get-deps.py
@@ -7,6 +7,9 @@ version="2024.3.2"
 
 import platform
 import subprocess
+import os
+import json
+import requests
 
 depdir = ""
 
@@ -32,11 +35,8 @@ for i in dependencies:
 
 
 
-import os
-import json
-
 # Only gets local copies of vendordep files for depdir wpilib/2024/maven
-def get_vendor_deps():
+def get_local_vendor_deps():
 	if platform.system() == "Windows" or platform.system().find("MSYS") != -1:
 		depdir = "C:/Users/Public/wpilib/2024/maven/"
 	else:
@@ -68,10 +68,77 @@ def get_vendor_deps():
 				print("Could not import local vendordep %s (cp failed)" % (groupId))
 			if status == 0:
 				print("Successfully imported local vendordep %s %s" % (groupId, artifactId))
+
 		for entry in data['jniDependencies']:
 			break
 		for entry in data['cppDependencies']:
 			break
 
 
-get_vendor_deps()
+def get_online_vendor_deps():
+	# Chose this directory because thats where the WPIlib plugin puts them by default
+	vendordeps_json_dir = os.fsencode(os.getcwd() + "/vendordeps")
+	for file in os.listdir(vendordeps_json_dir):
+		filename = os.fsdecode(file)
+		if not filename.endswith(".json"):
+			continue
+		
+		f = open(os.getcwd() + "/vendordeps/" + filename)
+
+		data = json.load(f)
+		base = data['mavenUrls'][0]
+
+		for entry in data['javaDependencies']:
+			if 'isJar' in entry and entry['isJar'] == False:
+				print("Spec says this isn't a .jar file, moving on")
+				continue
+			groupId = str(entry['groupId'])
+			artifactId = str(entry['artifactId'])
+			version = str(entry['version'])
+
+			url = gav_to_url(base, groupId, artifactId, version, "jar")
+			print("jar url = ", url)
+
+
+			#status = subprocess.call(
+				#"curl -s " + url 
+				#"./lib/%s-%s.jar" % (artifactId, version)	
+				#,
+				#shell=True
+			#)
+			perform_download(url)
+
+def perform_download(url):
+	'''
+	Do the downlaod of the file
+	:param url: url of the file to be downloaded
+	:return:  downloaded file       
+	'''
+	
+	filename = './lib/' + url.split("/")[-1] # Location of jar outputs
+	#print("Filename = ", filename)
+
+	try:
+		response = requests.get(url, allow_redirects=True)
+		open(filename, "wb").write(response.content)    # overwritting file in case it exists
+	except BaseException as be:
+		print(f"Something went wrong while downloading: {url} and the following exception was raised: {be}. Exiting!"
+        	f"This operation is mandatory, exiting!")
+		exit(1)
+	return filename
+
+def gav_to_url(base, g, a, v, ext):
+        '''
+        This method creates the JAR or POM file link from G:A:V coordinates
+        :param g: groupID
+        :param a: artefactID
+        :param v: artefact version
+        :param ext: set pom if you want to create a link for the pom file,
+                    set jar if you want to create a link for the jar file
+        :return: url pointing to the desired file: jar or pom
+        '''
+        
+        gid = g.replace(".", "/")
+        return base + gid + "/" + a + "/" + v + "/" + a + "-" + v + "." + ext
+
+get_online_vendor_deps()
